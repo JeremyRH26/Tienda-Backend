e5238ba8d009146e1bf28506f2f34287e680883f
Jeremy <jeremyramos150@gmail.com>
2026-04-11 22:32:48 -0600

    added expenses routes and removed commented code from sales routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const authRoutes = require('./auth.routes')
 const employeesRoutes = require('./employees.routes')
 const rolesRoutes = require('./roles.routes')
 const permissionsRoutes = require('./permissions.routes')
+const expensesRoutes = require('./expenses.routes')
 
 //importacion de rutas
 router.use('/auth', authRoutes)
 router.use('/employees', employeesRoutes)
+router.use('/expenses', expensesRoutes)
 router.use('/sales', salesRoutes)
 router.use('/roles', rolesRoutes)
 router.use('/permissions', permissionsRoutes)
--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const salesController = require('../controllers/sales.controller')
 
-//rutas final y llamar al controlador
 router.post('/', salesController.createSale)
 
 module.exports = router
